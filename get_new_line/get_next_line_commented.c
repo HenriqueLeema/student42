b838150704c7066fcfd26c684c67002904195345
Henrique De Souza De Lima <hde-souz@c1r9s1.42porto.com>
2023-11-16 00:09:41 +0000

    new commented version

--- get_new_line/get_next_line_commented.c
+++ get_new_line/get_next_line_commented.c
@@ -6,127 +6,127 @@
 /*   By: hde-souz <hde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 19:07:11 by hde-souz          #+#    #+#             */
-/*   Updated: 2023/11/15 22:57:04 by hde-souz         ###   ########.fr       */
+/*   Updated: 2023/11/16 00:08:36 by hde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-/*	This function takes a new line, a buffer, the buffer size, and a pointer to the line length.
-	It concatenates the new line with the buffer and returns the concatenated line.
-*/
-
+// Function to parse the current line and concatenate it with the existing buffer
 char	*parse_line(char *new_line, char *buffer, int buff_size, int *line_len)
 {
-    // Declare a pointer to the previous line
-    char	*line;
+	// Declare variables
+	char	*line;
+	int index = 0;
+	int size_line = 0;
+	int buffer_index = 0;
 
-    // Declare variables to keep track of the current index in the new line and the buffer
-    auto int index = 0;
-    auto int size_line = 0;
-    auto int buffer_index = 0;
+	// Point 'line' to the start of the current line
+	line = new_line;
 
-    // Set the previous line to the new line
-    line = new_line;
+	// Update the total line length
+	*line_len += buff_size;
 
-    // Update the line length to include the buffer size
-    *line_len += buff_size;
+	// Allocate memory for the new line
+	new_line = malloc(*line_len + 1 * sizeof(char));
 
-    // Allocate memory for the new line
-    new_line = malloc(*line_len + 1 * sizeof(char));
+	if (new_line)
+	{
+		// Set the end of the new line
+		new_line[*line_len] = 0;
 
-    // If memory allocation was successful, concatenate the previous line and the buffer
-    if (new_line)
-    {
-        // Set the last character of the new line to null byte
-        new_line[*line_len] = 0;
+		// Copy the existing line into the new_line buffer
+		while (line && line[size_line])
+			new_line[index++] = line[size_line++];
 
-        // Copy the previous line to the new line
-        while (line && line[size_line])
-            new_line[index++] = line[size_line++];
+		// Reset size_line for buffer processing
+		size_line = 0;
 
-        // Copy the buffer to the new line
-        size_line = 0;
-        while (buffer && buffer[size_line])
-        {
-            if (size_line < buff_size)
-                new_line[index++] = buffer[size_line];
-            else
-                buffer[buffer_index++] = buffer[size_line];
-            buffer[size_line++] = 0;
-        }
-    }
+		// Copy the buffer content into the new_line buffer
+		while (buffer && buffer[size_line])
+		{
+			if (size_line < buff_size)
+				new_line[index++] = buffer[size_line];
+			else
+				buffer[buffer_index++] = buffer[size_line];
 
-    // Free the previous line and return the new line
-    free(line);
-    return (new_line);
+			// Set the current buffer element to 0 (null character)
+			buffer[size_line++] = 0;
+		}
+	}
+
+	// Free the old line buffer
+	free(line);
+
+	// Return the new_line buffer && handles null malloc
+	return (new_line);
 }
 
-/*	This function reads a file descriptor and returns the next line of the file.
-	It takes the file descriptor, a buffer, and a pointer to the buffer size.
-	It reads the buffer and searches for the next newline character.
-	If it finds a newline character, it calls the parse_line function to extract the line from the buffer and return it.
-	If it does not find a newline character, it continues to read the file descriptor into the buffer until it finds one.
-*/
-
+// Function to read from the file descriptor and retrieve the next line
 char	*read_bf(int fd, char buffer[][BUFFER_SIZE + 1], int *sz_bf)
 {
-    auto int flag = 1;
-    auto int size_line = 0;
-    auto char *line = NULL;
-    while (flag > 0)
-    {
-        // If the buffer is empty, read from the file descriptor
-        if (!buffer[fd][0])
-            *sz_bf = read(fd, buffer[fd], BUFFER_SIZE);
-        flag = *sz_bf;
-        if (*sz_bf > 0)
-        {
-            // Search for the next newline character in the buffer
-            *sz_bf = 0;
-            while (buffer[fd][*sz_bf] && buffer[fd][*sz_bf] != '\n')
-                (*sz_bf)++;
-            // If the character at the current position in the buffer is a newline character,
-            // then increment 'sz_bf' by 1 to condition the loop to read the next line.
-            *sz_bf += buffer[fd][*sz_bf] == '\n';
-            // Parse the current line and update the size of the line
-            line = parse_line(line, buffer[fd], *sz_bf, &size_line);
-        }
-        // If read returned -1 to sz_bf, we handle the error by
-        // freeing the memory alocated to 'line' and setting is as null
-        // so that we return null upon error
-        else if (*sz_bf == -1)
-        {
-            free(line);
-            line = NULL;
-        }
-    }
-    return (line);
+	// Declare variables
+	int flag = 1;
+	int size_line = 0;
+	char *line = NULL;
+
+	// Loop until the end of file or newline character is encountered
+	while (flag > 0)
+	{
+		// If the current buffer is empty, read from the file descriptor
+		if (!buffer[fd][0])
+			*sz_bf = read(fd, buffer[fd], BUFFER_SIZE);
+
+		// Update the flag with the current buffer size
+		flag = *sz_bf;
+
+		// If there are characters in the buffer
+		if (*sz_bf > 0)
+		{
+			*sz_bf = 0;
+
+			// Find the position of the newline character or reach the end of buffer
+			while (buffer[fd][*sz_bf] && buffer[fd][*sz_bf] != '\n')
+				(*sz_bf)++;
+
+			// Update the flag based on whether a newline character was found
+			flag = (flag == *sz_bf);
+
+			// Include the newline character in the count and parse the line
+			*sz_bf += buffer[fd][*sz_bf] == '\n';
+			line = parse_line(line, buffer[fd], *sz_bf, &size_line);
+		}
+		// If there was an error reading from the file descriptor
+		else if (*sz_bf == -1)
+		{
+			// Free the line buffer and set it to NULL
+			free(line);
+			line = NULL;
+		}
+	}
+	// Return the parsed line
+	return (line);
 }
 
-/*	This function initializes a static buffer for each file descriptor and reads the file descriptor into the buffer.
-	It then calls the read_bf function to parse the buffer and return the next line of the file.
-*/
+// Function to get the next line from a given file descriptor
 char	*get_next_line(int fd)
 {
-    // Declare a static buffer for each file descriptor
-    static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
+	// Static buffer to store data for each file descriptor
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
 
-    // Declare a variable to keep track of the number of characters read into the buffer
-    auto int sz_bf = 0;
+	// Initialize buffer size variable
+	int sz_bf = 0;
 
-    // Handle invalid file descriptor input by returning NULL
-    if (fd < 0 || fd > FOPEN_MAX)
-        return (NULL);
+	// Check if the file descriptor is valid
+	if (fd < 0 || fd > FOPEN_MAX)
+		return (NULL);
 
-    // Iterate through the buffer for the specified file descriptor until it reaches the end of the buffer or encounters a null byte
-    // This allows the function to determine how many characters have already been read into the buffer for that file descriptor
-    // so that it can continue reading from where it left off
-    while (buffer[fd][0])
-        sz_bf++;
+	// Find the size of the existing buffer for the given file descriptor
+	while (buffer[fd][sz_bf])
+		sz_bf++;
 
-    // Call the read_bf function to read the file descriptor and return the next line of the file
-    return (read_bf(fd, buffer, &sz_bf));
+	// Read and return the next line from the file descriptor
+	return (read_bf(fd, buffer, &sz_bf));
 }
 
 
@@ -134,7 +134,7 @@ char	*get_next_line(int fd)
 
 
 
-int main(int argc, char **argv)
+/* int main(int argc, char **argv)
 {
     char *line;
 
@@ -158,5 +158,36 @@ int main(int argc, char **argv)
     }
     else
         fprintf(stderr, "Wrong call! Try: %s file1 file2 file3 ...\n", argv[0]);
+    return 0;
+} */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line_bonus.h"
+
+int main()
+{
+    // List of file names to test
+    char *file_names[] = {"test1.txt", "test2.txt"};
+
+    for (int i = 0; i < sizeof(file_names) / sizeof(file_names[0]); i++)
+    {
+        int fd = open(file_names[i], O_RDONLY);
+        if (fd == -1)
+        {
+            perror("Error opening file");
+            continue;
+        }
+
+        char *line;
+        while ((line = get_next_line(fd)))
+        {
+            printf("%s", line);
+            free(line);
+        }
+
+        close(fd);
+    }
+
     return 0;
 }
